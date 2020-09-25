222f0b016a9625ddeed1b70ea0857d76d7a25165
Mecke Dev <crafted.mecke@gmail.com>
2020-09-26 00:15:52 +0200

    init

---
+++ main.py
@@ -0,0 +1,46 @@
+# bot.py
+import os
+import random
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+@bot.event
+async def on_message(message):
+    if message.author.name == "Twitchcord":
+        return
+
+    else:
+
+        e = discord.Embed(title='Test')
+        await message.channel.send(message.content, embed=e)
+        
+        await bot.process_commands(message)
+
+@bot.command(name='roll_dice', help='Simulates rolling dice.')
+async def roll(ctx, number_of_dice: int, number_of_sides: int):
+    dice = [
+        str(random.choice(range(1, number_of_sides + 1)))
+        for _ in range(number_of_dice)
+    ]
+    await ctx.send(', '.join(dice))
+
+@bot.command(name='create-channel')
+@commands.has_role('admin')
+async def create_channel(ctx, channel_name='real-python'):
+    guild = ctx.guild
+    existing_channel = discord.utils.get(guild.channels, name=channel_name)
+    if not existing_channel:
+        print(f'Creating a new channel: {channel_name}')
+        await guild.create_text_channel(channel_name)
+
+bot.run(TOKEN)
